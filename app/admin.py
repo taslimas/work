ee96dab46066830b41ac70f4711da01aceda975b
Taslima S <tasliarshad11@gmail.com>
2022-12-31 21:32:23 +0530

    models added

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+admin.site.register(Role)
+@admin.register(User)
+class UserModelAdmin(admin.ModelAdmin):
+
+    list_display=['roles']
--- app/models.py
+++ app/models.py
@@ -1,3 +1,22 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+class Role(models.Model):
+    
+    
+    ROLE_CHOICES = (
+      (1, ' staff'),
+      (2, ' doctor'),
+      (3, ' nurse'),
+      (4, 'manager'),
+      (5, 'admin'),
+  )
+    id = models.PositiveSmallIntegerField(choices=ROLE_CHOICES, primary_key=True)  
+   
+    def __str__(self):
+      return self.get_id_display()
+class User(models.Model):
+    roles = models.ForeignKey(Role, on_delete=models.CASCADE)
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    def __str__(self):
+         return self.roles
